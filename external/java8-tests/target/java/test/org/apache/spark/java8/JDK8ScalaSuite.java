4cf1922d6759179744cfd6760dd06f648d341dfc
hongyao.zhao <andyrao1986@gmail.com>
2016-09-28 19:15:50 +0800

    spark 1.6.2

---
+++ external/java8-tests/target/java/test/org/apache/spark/java8/JDK8ScalaSuite.java
@@ -0,0 +1,7 @@
+package test.org.apache.spark.java8;
+/**
+ * Test cases where JDK8-compiled Scala user code is used with Spark.
+ */
+public  class JDK8ScalaSuite extends org.apache.spark.SparkFunSuite implements org.apache.spark.SharedSparkContext {
+  public   JDK8ScalaSuite ()  { throw new RuntimeException(); }
+}
